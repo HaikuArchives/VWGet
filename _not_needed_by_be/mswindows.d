8d79c2a00e1527823739c8a21f913e8c3d618200
Josef Gajdusek <atx@atx.name>
2014-12-28 20:31:17 +0100

    Remove not needed files

--- _not_needed_by_be/mswindows.d
+++
@@ -1,5 +0,0 @@
-mswindows.o: /boot/fichiers/prg/VisualWget/src/mswindows.c \
- /boot/fichiers/prg/VisualWget/src/wget.h \
- /boot/fichiers/prg/VisualWget/src/sysdep.h \
- /boot/fichiers/prg/VisualWget/src/options.h \
- /boot/fichiers/prg/VisualWget/src/url.h
